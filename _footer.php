12f8e4e9d997c4626eb87e57232f9e4e700ca300
Paul Derivaux <issy_paul@hotmail.fr>
2016-03-16 16:29:06 +0100

    add footer

--- _footer.php
+++ _footer.php
@@ -1,5 +1,58 @@
-<footer class="bg_noir">
+<footer class="bg_noir text-white">
     <div class="container">
-        <img src="img/pied_de_page.png" class="img-responsive">
+        <div class="col-md-3">
+            <h4>Le mot des créateurs</h4>
+            <p class="text-muted">L'idée de ce site est née..<br>
+                blablabla<br>
+                blablabla<br>
+                blablabla</p>
+            <h4>Qui Sommes-nous ?</h4>
+            <p class="text-muted">Notre équipe a vu le jour..<br>
+                blablabla<br>
+                blablabla<br>
+                blablabla</p>
+            <a class="text-muted">En savoir plus</a>
+        </div>
+        <div class="col-md-6 text-center">
+            <h4>Nous contacter</h4>
+            <form>
+                <div class="col-xs-6">
+                    <div class="form-group">
+                        <input type="text" name="nom" id="nom" class="form-control" placeholder="Nom Prénom">
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="form-group">
+                        <input type="email" name="mail" id="mail" class="form-control" placeholder="Adresse Mail">
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <div class="form-group">
+                        <textarea name="message" id="message" class="form-control" placeholder="Message" rows=6"></textarea>
+                    </div>
+                </div>
+            </form>
+            <h4>Liens utiles</h4>
+            <a class="text-muted">Site ENSAM</a><br>
+            <a class="text-muted">Site UE</a>
+        </div>
+        <div class="col-md-3 text-right">
+            <h4>Plan du site</h4>
+            <h5 class="text-uppercase text-rouge">Logement</h5>
+                <a class="text-muted">Résidences</a><br>
+                <a class="text-muted">Appartements</a><br>
+                <a class="text-muted">Services</a>
+            <h5 class="text-uppercase text-orange">Campus</h5>
+                <a class="text-muted">Campus</a><br>
+                <a class="text-muted">Vie Pratique</a>
+            <h5 class="text-uppercase text-vert">Vie Etudiante</h5>
+                <a class="text-muted">Associations</a><br>
+                <a class="text-muted">Sport</a><br>
+                <a class="text-muted">Evenements</a>
+            <h5 class="text-uppercase text-bleu">Etudes</h5>
+                <a class="text-muted">Cursus</a><br>
+                <a class="text-muted">Ecole</a><br>
+                <a class="text-muted">International</a>
+        </div>
     </div>
 </footer>
