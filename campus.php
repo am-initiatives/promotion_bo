4fa71a62fcc37a76df77efc7f240506feeca655c
issy_paul@hotmail.fr <floflo92>
2016-04-20 13:04:31 +0200

    add map for campus

--- campus.php
+++ campus.php
@@ -30,34 +30,15 @@
             <div class="row">
                 <h2 class="text-uppercase text-couleur">Le domaine universitaire</h2>
                 <p class="text-muted text-justify">L’Ecole des Arts et Métiers de Bordeaux-Talence est implantée sur le domaine universitaire Talence-Pessac-Gradignan, plus grand campus de l’agglomération bordelaise. Il accueille environ 65 000 étudiants, 5 000 chercheurs et enseignants et 3 000 membres du personnel. D’une superficie totale de 235 hectares, il s’agit du deuxième plus grand campus de France après Paris-Sud. Le tramway (ligne B) traverse le campus par les stations Peixotto, Béthanie, Arts et Métiers, François Bordes, Doyen Brus, Montaigne Montesquieu et UNITEC.</p>
-                <img src="img/campus/campus.jpg" class="">
+                <img src="img/campus/campus.jpg" class="img-responsive">
             </div>
         </div>
     </section>
 
     <section id="section_ecoles">
         <div class="container">
-                <h2 class="text-uppercase text-couleur text-right">Les écoles présentes sur la campus</h2>
-            <div class="row text-center">
-                <img src="img/campus/logos_gris/ensap.jpg" class="col-md-2 col-md-offset-1">
-                <img src="img/campus/logos_gris/inp.jpg" class="col-md-2">
-                <img src="img/campus/logos_gris/optique.jpg" class="col-md-2">
-                <img src="img/campus/logos_gris/isvv.jpg" class="col-md-2">
-                <img src="img/campus/logos_gris/iut.jpg" class="col-md-2">
-            </div>
-            <div class="row">
-
-            </div>
-            <div class="row text-center">
-                <img src="img/campus/logos_gris/kedge.jpg" class="col-md-2 col-md-offset-1">
-                <img src="img/campus/logos_gris/agro.jpg" class="col-md-2">
-                <img src="img/campus/logos_gris/sciencespo.jpg" class="col-md-2">
-                <img src="img/campus/logos_gris/montaigne.jpg" class="col-md-2">
-                <img src="img/campus/logos_gris/universite.jpg" class="col-md-2">
-            </div>
-            <div class="row">
-
-            </div>
+            <h2 class="text-uppercase text-couleur text-right">Les écoles présentes sur le campus universitaire</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11324.579587075137!2d-0.604891800815385!3d44.798235617418264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x4331235e25afe43d!2sArts+et+M%C3%A9tiers+ParisTech!5e0!3m2!1sfr!2sfr!4v1459252862402" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </section>
 
