1169725786360126de77a90045827e5df6972d82
issy_paul@hotmail.fr <floflo92>
2016-05-10 19:05:41 +0200

    adapt content

--- _footer.php
+++ _footer.php
@@ -1,6 +1,6 @@
 <footer class="bg_noir text-white">
     <div class="container">
-        <div class="col-md-3 col-xs-8">
+        <div class="col-md-3 col-xs-12">
             <h4>Le mot des créateurs</h4>
             <p class="text-muted text-justify">L'idée de ce site est née d'une envie de transparence totale vis-à-vis des étudiants et de leur cercle proche.
                 Ici, on explique ce que l'on fait tous les jours, pourquoi on le fait, et comment on s'y prend. Rien de plus!
@@ -9,25 +9,6 @@
             <p class="text-muted text-justify">Une équipe soudée et créative de Cinq élèves ingénieurs du Campus des Arts et Métiers de Bordeaux, aux talents variés.
                 La diversité de nos compétences nous a permis de développer ce site, tous animés par l'envie de partager et d'informer en toute simplicité.</p>
             <!--<a class="text-muted">En savoir plus</a>-->
-        </div>
-        <div class="col-xs-4 hidden-md hidden-lg text-right">
-            <h4>Plan du site</h4>
-            <h5 class="text-uppercase text-rouge">Logement</h5>
-            <a href="logement.php#section_residences" class="text-muted">Résidences</a><br>
-            <a href="logement.php#section_appartements" class="text-muted">Appartements</a><br>
-            <a href="logement.php#section_services" class="text-muted">Services</a>
-            <h5 class="text-uppercase text-orange">Campus</h5>
-            <a href="campus.php" class="text-muted">Campus</a><br>
-            <a href="vie_pratique.php" class="text-muted">Vie Pratique</a><br>
-            <a href="bordeaux.php" class="text-muted">Bordeaux</a>
-            <h5 class="text-uppercase text-vert">Vie Etudiante</h5>
-            <a href="associations.php" class="text-muted">Associations</a><br>
-            <a href="sport.php" class="text-muted">Sport</a><br>
-            <a href="evenements.php" class="text-muted">Evenements</a>
-            <h5 class="text-uppercase text-bleu">Etudes</h5>
-            <a href="cursus.php#section_contexte" class="text-muted">Contexte</a><br>
-            <a href="cursus.php#section_cursus" class="text-muted">Cursus</a><br>
-            <a href="international.php" class="text-muted">International</a>
         </div>
         <div class="col-md-6 col-xs-12 text-center">
             <h4>Nous contacter</h4>
@@ -49,15 +30,15 @@
                 </div>
             </form>
             <h4>Liens utiles</h4>
-            <div class="col-xs-3">
+            <div class="col-sm-3 col-xs-12">
                 <a class="col-xs-6" href="https://twitter.com/AM_ParisTech"><i class="fa fa-3x fa-twitter"></i></a>
                 <a class="col-xs-6" href="https://www.facebook.com/AMParisTech/?fref=ts&rf=111762148842866"><i class="fa fa-3x fa-facebook"></i></a>
             </div>
-            <a class="col-xs-3" href="http://www.ueensam.org/"><img src="img/footer/ue.jpg"></a>
-            <a class="col-xs-3" href="http://www.arts-et-metiers.asso.fr/"><img src="img/footer/soce.jpg"></a>
-            <a class="col-xs-3" href="http://www.ensam.eu/"><img src="img/footer/ensam.jpg"></a>
+            <a class="col-sm-3 col-xs-12" href="http://www.ueensam.org/"><img src="img/footer/ue.jpg"></a>
+            <a class="col-sm-3 col-xs-12" href="http://www.arts-et-metiers.asso.fr/"><img src="img/footer/soce.jpg"></a>
+            <a class="col-sm-3 col-xs-12" href="http://www.ensam.eu/"><img src="img/footer/ensam.jpg"></a>
         </div>
-        <div class="col-md-3 hidden-xs hidden-sm text-right">
+        <div id="plan_site" class="col-md-3 col-xs-12 text-right">
             <h4>Plan du site</h4>
             <h5 class="text-uppercase text-rouge">Logement</h5>
                 <a href="logement.php#section_residences" class="text-muted">Résidences</a><br>
