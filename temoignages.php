9f1962917d159c01a0157a36401b9875b21705ca
Paul Derivaux <issy_paul@hotmail.fr>
2016-05-17 23:13:28 +0200

    add the temoignages content

--- temoignages.php
+++ temoignages.php
@@ -34,9 +34,127 @@
         </div>
     </section>
 
-    <section id="section_temoignages">
+    <section id="section_temoignages" class="text-muted">
         <div class="container">
-
+            <div class="col-xs-12">
+                <div class="col-md-5 temoignage">
+                    <div class="row titre">
+                        <img src="" class="col-xs-3 img-circle">
+                        <div class="col-xs-9 text-center">
+                            <h2>Vincent</h2>
+                            <p><b>Elève ingénieur 1ère année</b><br>
+                            Issu de classe préparatoire</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fa fa-quote-left fa-3x text-couleur col-xs-2"></i>
+                        <div class="col-xs-10 text-justify contenu">
+                            <p>Les premiers mois de cours sont un peu troublant. J’ai mis du temps à comprendre ce que les profs attendaient de nous. Mais ensuite on s’adapte au fonctionnement.<br><br>
+                                J’aime beaucoup le format de cours TP/TD : l’interaction avec les profs est plus facile, c’est grâce à ces cours que l’on apprend. En plus, les TP en atelier sont ludiques et vraiment différents de ce qu’on a pu faire en prépa.<br><br>
+                                Les projets sont aussi très enrichissant, ils permettent de s’impliquer personnellement et de développer le travail d’équipe. C’est dans les projets que l’on découvre vraiment le travail d’ingénieur, mêlant rigueur du cahier des charges et créativité de la conception.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 centre"><p></p></div>
+                <div class="col-md-5 temoignage">
+                    <div class="row titre">
+                        <img src="" class="col-xs-3 img-circle">
+                        <div class="col-xs-9 text-center">
+                            <h2>Xavier</h2>
+                            <p><b>Elève ingénieur 3ème année</b><br>
+                                Actuellement en double-diplôme avec l'Universidad El Bosque - Colombie</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fa fa-quote-left fa-3x text-couleur col-xs-2"></i>
+                        <div class="col-xs-10 text-justify contenu">
+                            <p>Aux Arts, il y a tellement de cursus proposés que chacun peut y trouver son compte. J’avais envie de partir loin, j’ai donc profité d’un accord d’échange pour partir en Colombie. C’était l’occasion d’élargir mon horizon et de m’enrichir au contact d’une culture différente.<br><br>
+                                Je ne regrette pas mon départ, j’adore mon parcours et l’endroit où je vis ! Mon cursus m’offre la possibilité de réaliser un stage de 6 mois en entreprise, ce qui est un atout dans le CV. Les stages sont l’opportunité de découvrir le monde du travail dans son ensemble.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <div class="col-md-5 temoignage">
+                    <div class="row titre">
+                        <img src="" class="col-xs-3 img-circle">
+                        <div class="col-xs-9 text-center">
+                            <h2>Jules</h2>
+                            <p><b>Elève ingénieur 3ème année</b><br>
+                                Issu de classe préparatoire</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fa fa-quote-left fa-3x text-couleur col-xs-2"></i>
+                        <div class="col-xs-10 text-justify contenu">
+                            <p>Le stage ouvrier est très important, car il te met au cœur de l’entreprise, au plus bas de la hiérarchie. C’est primordial de comprendre les enjeux sociaux et les interactions entre les personnes pour vouloir plus tard diriger des équipes.<br><br>
+                                Ce stage donne l’opportunité de découvrir le monde de l’entreprise, de se faire une idée du rythme de travail, des coûts, de la sécurité dans une usine.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 centre"><p></p></div>
+                <div class="col-md-5 temoignage">
+                    <div class="row titre">
+                        <img src="" class="col-xs-3 img-circle">
+                        <div class="col-xs-9 text-center">
+                            <h2>Clément</h2>
+                            <p><b>Elève ingénieur 2ème année</b><br>
+                                Issu de classe préparatoire</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fa fa-quote-left fa-3x text-couleur col-xs-2"></i>
+                        <div class="col-xs-10 text-justify contenu">
+                            <p>Aux Arts, les professeurs sont au point dans leur domaine. Ils sont accessibles et disponibles pour des questions ou des approfondissements.<br><br>
+                                Venant de l’ESTP, je n’ai pas eu de problème pour reprendre les cours directement en deuxième année. Le campus est super, avec beaucoup de possibilités techniques et scientifiques. Je découvrirai l’expertise aéronautique l’an prochain, mais les cours ont l’air intéressants et techniques.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <div class="col-md-5 temoignage">
+                    <div class="row titre">
+                        <img src="" class="col-xs-3 img-circle">
+                        <div class="col-xs-9 text-center">
+                            <h2>Milène</h2>
+                            <p><b>Elève ingénieur 2ème année</b><br>
+                                Actuellement en double-diplôme avec l'Universidad El Bosque - Colombie</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fa fa-quote-left fa-3x text-couleur col-xs-2"></i>
+                        <div class="col-xs-10 text-justify contenu">
+                            <p>Les cours enseignés sont très divers et la formation est reconnue dans le monde professionnel. Les matières comme le management, la communication ou l’organisation d’entreprises sont des notions nouvelles qui nous apprennent beaucoup sur le contexte dans lequel va se dérouler une grande partie de notre vie.<br><br>
+                                Si j’avais quelque chose à reprocher au cursus scolaire, ce serait le manque de formation sur le digital, qui envahit de plus en plus le marché du travail.<br><br>
+                                L’an prochain, je pars à Paris faire le cursus Michel Serres. J’ai choisi cette formation car j’ai développé la culture des projets innovants aux Arts et Métiers, le besoin de révolutionner notre quotidien et de répondre aux défis contemporains de notre société.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 centre"><p></p></div>
+                <div class="col-md-5 temoignage">
+                    <div class="row titre">
+                        <img src="" class="col-xs-3 img-circle">
+                        <div class="col-xs-9 text-center">
+                            <h2>Xavier</h2>
+                            <p><b>Elève ingénieur 3ème année</b><br>
+                                Actuellement en double-diplôme avec l'Universidad El Bosque - Colombie</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fa fa-quote-left fa-3x text-couleur col-xs-2"></i>
+                        <div class="col-xs-10 text-justify contenu">
+                            <p>Aux Arts, il y a tellement de cursus proposés que chacun peut y trouver son compte. J’avais envie de partir loin, j’ai donc profité d’un accord d’échange pour partir en Colombie. C’était l’occasion d’élargir mon horizon et de m’enrichir au contact d’une culture différente.<br><br>
+                                Je ne regrette pas mon départ, j’adore mon parcours et l’endroit où je vis ! Mon cursus m’offre la possibilité de réaliser un stage de 6 mois en entreprise, ce qui est un atout dans le CV. Les stages sont l’opportunité de découvrir le monde du travail dans son ensemble.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
