977a717afd59a2cedd659b7b36144a4d0c2e17bb
Paul Derivaux <issy_paul@hotmail.fr>
2016-05-18 15:11:24 +0200

    add content on events

--- evenements.php
+++ evenements.php
@@ -39,6 +39,10 @@
                         <p>Que tu aimes organiser, décorer ou être barman, tous les élèves ingénieurs participent à l’élaboration de la soirée.  Alors rejoins nous dans cette aventure, et participer à l’une des meilleures nuits bordelaises !</p>
                         <p>Pour en savoir plus : <a href="http://ggbo.gadz.org">http://ggbo.gadz.org</a></p>
                     </div>
+                    <div class="col-xs-12 text-center temoignages">
+                        <p><i>« Le gala, c’est la fierté de voir sa famille bouche bée devant la beauté de nos réalisations. Une fierté sans pareille. »</i></p>
+                        <p><i>« Les valeurs chères aux Gadzarts prennent tout leur sens lors de la préparation du gala. Le gala, c’est l’aboutissement d’un travail collectif de 300 étudiants, qui procure une grande satisfaction pour tous ceux qui participent à la préparation. »</i></p>
+                    </div>
                 </div>
                 <div class="row bg_sombre text-white text-justify">
                     <img src="img/evenements/ve.jpg" class="col-md-6 col-xs-12">
@@ -47,6 +51,10 @@
                         <p>Que serait la vie aux Arts et Métiers sans ses célèbres Soirées VE ? Le principe est simple : tu invites tes amis, tu viens et tu t’amuses !</p>
                         <p>La Vie Étudiante des Arts et Métiers est le pôle évènementiel de l’école. Il se charge de te transmettre les bons plans de sorties en ville ou dans les écoles voisines ; mais surtout d’organiser et d’animer des soirées et apéros toute l’année.</p>
                         <p>Que ce soit pour la Saint Patrick ou à Noël, la VE ne manque jamais une occasion de rassembler le tissu étudiant local pour faire la fête avec les DJ du campus. Alors, tu viens quand ?</p>
+                    </div>
+                    <div class="col-xs-12 text-center temoignages">
+                        <p><i>« Les soirées VE, ce sont toujours des soirées hautes en couleurs. Participer à leur préparation est toujours un bon moment pour moi. Et la soirée est encore meilleure ensuite ! C’est l’occasion de retrouver les étudiants du campus, et toutes les écoles ne le font pas. »</i></p>
+                        <p><i>« L’école ne permet pas d’accueillir dans une salle, et c’est ce qui donne toute l’originalité des soirées VE. Dès le printemps, c’est toujours cool de faire venir les étudiants du campus et de discuter autour d’une bière sous les étoiles. »</i></p>
                     </div>
                 </div>
                 <div class="row bg_blanc text-muted text-justify">
@@ -57,6 +65,10 @@
                         <p>Au programme pour les sportifs, tous types de sport, du rugby à la pétanque en passant par les sports à raquette, la natation, l’athlétisme et l’inimitable concours de pompom girls !</p>
                         <p>Tu n’es pas sportif ? Viens encourager les équipes de ton campus et participer aux animations du week-end : babyfoot humains, concerts, soirées, concours en tout genre. Retrouve les Gadzarts de toute la France pour te plonger dans une ambiance hors du commun !</p>
                     </div>
+                    <div class="col-xs-12 text-center temoignages">
+                        <p><i>« Les UAI sont d’une rare intensité, physique et émotionnelle. En réunissant plusieurs promotions issues de toute la France, le tournoi est l’occasion d’épreuves sportives haletantes, encouragées par des supporters de folie. »</i></p>
+                        <p><i>« Les UAI ? Un rassemblement sportif dans un esprit caractéristique de la communauté Arts et Métiers, pour les amateurs de sports festifs et de fêtes sportives ! »</i></p>
+                    </div>
                 </div>
                 <div class="row bg_blanc text-muted text-justify">
                     <img src="img/evenements/skz.jpg" class="col-md-6 col-xs-12">
@@ -64,6 +76,10 @@
                         <h2 class="text-right text-uppercase">SKZ</h2>
                         <p>Ski’O’Zarts, c’est une semaine de ski et de fête pour tous ! Plus de 1200 étudiants se retrouvent dans une station des Alpes durant une semaine à un prix défiant toute concurrence !</p>
                         <p>Que tu sois débutant ou expérimenté et avide de sensations, les responsables s’occupent de tout pour te permettre de profiter un maximum : compétitions pour les meilleurs, cours d’initiations pour ceux qui le souhaitent. Après une descente aux flambeaux, les bars de la station t’attendent pour des soirées spéciales pleines de surprises !</p>
+                    </div>
+                    <div class="col-xs-12 text-center temoignages">
+                        <p><i>« Idéal pour ceux qui veulent découvrir le ski pour un budget TRES réduit ! Sans doute l’une des meilleures semaines de ma vie ! »</i></p>
+                        <p><i>« SKZ, c’est un peu comme un SprinkBreak ! Il y en a pour tout le monde, les animations organisées chaque jour permettent à ceux qui ne skient pas de se faire plaisir. L’ambiance est incroyable sur les pistes et en dehors ! »</i></p>
                     </div>
                 </div>
                 <div class="row bg_blanc text-muted text-justify">
@@ -73,6 +89,9 @@
                         <p>La croisière AM, c’est une occasion unique de partir faire un trip avec ta bande ! Un voilier pour dix personnes, les criques de Croatie, des soirées, que demander de plus ?</p>
                         <p>Une équipe s’occupe de réserver les bateaux et de t’organiser le voyage, avec kit de nourriture et transport. Tu n’as plus qu’à profiter de ce cadre de rêve ! La seule règle ? Que l’un de vous soit skipper. En étant à Bordeaux, tu auras toute l’année pour passer les certifications et sortir naviguer.</p>
                         <p>Alors, une semaine de chill sur les eaux turquoise, ça te tente ?</p>
+                    </div>
+                    <div class="col-xs-12 text-center temoignages">
+                        <p><i>« L’un de mes meilleurs moments aux Arts. Profiter du soleil, des dauphins, du rhum, le tout dans un cadre idyllique. Même pas besoin d’avoir déjà navigué pour profiter ! Que demander de plus ? »</i></p>
                     </div>
                 </div>
             </div>
